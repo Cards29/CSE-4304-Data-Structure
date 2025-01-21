28d8c48ba292e8e3682803ad1fe93f43c1d8ef1d
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-01-21 07:58:44 +0600

    Lab 9 (not complete)

--- Lab 9/220041118_T02_L09_1B.cpp
+++ Lab 9/220041118_T02_L09_1B.cpp
@@ -7,77 +7,80 @@ using namespace std;
 
 struct node {
     bool endmark;
+    int count;
     node* next[52];
-    node(): endmark(false) {
-        for(int i=0;i<52;i++) next[i] = nullptr;
+    node() : endmark(false), count(0) {
+        for (int i = 0;i < 52;i++) next[i] = nullptr;
     }
 };
 
-class Trie{
+class Trie {
 private:
     node* root;
 public:
-    Trie(): root(new node) {}
-    void insert(string s){
+    Trie() : root(new node) {}
+    void insert(string s) {
+        if(search(s)) return;
         int n = s.size();
         node* current = root;
-        for(int i=0;i<n;i++){
-            // if (s[i] < 'a') s[i] = s[i] + 32;
+        for (int i = 0;i < n;i++) {
             int index = tolower(s[i]) - 'a';
-            if(current->next[index] == nullptr) current->next[index] = new node;
+            if (current->next[index] == nullptr) current->next[index] = new node;
             current = current->next[index];
+            current->count++;
         }
         current->endmark = true;
     }
-
-    int search(string s) {
+    bool search(string s) const {
         int n = s.size();
-        int prefixCount = 0;
         node* current = root;
-        for(int i=0;i<n;i++){
-            // if (s[i] < 'a') s[i] = s[i] + 32;
+        for (int i = 0;i < n;i++) {
             int index = tolower(s[i]) - 'a';
-            if(current->next[index] == nullptr) return 0;
+            if (current->next[index] == nullptr) return false;
             current = current->next[index];
         }
-        for(int i=0; i<26; i++) 
-            if(current->next[i] != nullptr) prefixCount++;
-        return prefixCount + current->endmark;
+        return current->endmark;
     }
-
     void display(node* current, string s = "") const {
-        // if(current == nullptr) return;
-        if(current->endmark){
-            // s += '\0';
+        if (current->endmark) {
             cout << s << " ";
         }
-        for(int i=0; i<52; i++) {
-            if(current->next[i] != nullptr){
+        for (int i = 0; i < 52; i++) {
+            if (current->next[i] != nullptr) {
                 s += (i + 'a');
                 display(current->next[i], s);
                 s.pop_back();
             }
         }
     }
-
+    int getCount(string s) const {
+        int n = s.size();
+        node* current = root;
+        for (int i = 0;i < n;i++) {
+            int index = tolower(s[i]) - 'a';
+            if (current->next[index] == nullptr) return 0;
+            current = current->next[index];
+        }
+        return current->count;
+    }
     node* getroot() const {
         return root;
     }
 };
 
-int main(){
+int main() {
     Trie t1;
     int n, q;
     cin >> n >> q;
-    while(n--){
+    while (n--) {
         string word;
         cin >> word;
         t1.insert(word);
     }
-    while(q--){
+    while (q--) {
         string word;
         cin >> word;
-        cout << t1.search(word) << endl;
+        cout << t1.getCount(word) << endl;
     }
 
     return 0;
